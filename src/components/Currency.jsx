cfab259ce1b0265b679ea9a15b3ccfa42b8fce1e
MiracleChibuike <miraclechibuike27@gmail.com>
2025-04-21 18:26:19 +0100

    updated the language preference and currency pref

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -14,7 +14,7 @@ const Currency = () => {
             backNav("/Dashboard");
         };
 
-        // Fetch data
+        // Fetch data Colombia Peso
         const [isFlagVisible, setIsFlagVisible] = useState("");
 
         useEffect(() => {
@@ -36,7 +36,51 @@ const Currency = () => {
             }
            };
            fetchData();
-        }, [])
+        }, []);
+
+        const [isFlagVisible2, setIsFlagVisible2] = useState("");
+        // 
+        useEffect(() => {
+            const dollarCurr = async () => {
+                try {
+                    const usdCurrency = await axios.get(
+                      "https://restcountries.com/v3.1/name/united%20states"
+                    );
+                    const usdId = usdCurrency.data[0].currencies;
+                    const usdFlag = usdCurrency.data[0].flags.png;
+                    const symbol = usdCurrency.data[0].currencies.USD.symbol;
+                    console.log(usdId);
+                    console.log(usdFlag);
+                    console.log(symbol);
+                   setIsFlagVisible2(usdFlag)
+                } catch (error) {
+                    
+                }
+            };
+            dollarCurr();
+        }, []);
+
+        // Fetch British Pounds
+        const [isFlagVisible3, setIsFlagVisible3] = useState("");
+        useEffect(() => {
+            const pounds = async () => {
+                try {
+                    const gbpCurrency = await axios.get(
+                      "https://restcountries.com/v3.1/name/united%20kingdom"
+                    );
+                    const gbpId = gbpCurrency.data[0].currencies;
+                    const gbpFlag = gbpCurrency.data[0].flags.png;
+                    const symbol = gbpCurrency.data[0].currencies.GBP.symbol;
+                    console.log(gbpId);
+                    console.log(gbpFlag);
+                    console.log(symbol);
+                   setIsFlagVisible3(gbpFlag)
+                } catch (error) {
+                    
+                }
+            };
+            pounds();
+        }, []);
   return (
     <>
       <Helmet>
@@ -62,7 +106,21 @@ const Currency = () => {
           </div>
           {/* CurrencyLists */}
           <div className="currencyLists">
-            <div className="currencyitem"></div>
+            {isFlagVisible2 && (
+              <div className="currencyitem">
+                <img src={isFlagVisible2} alt="" />
+                <div className="currencyDesc">
+                  <p>
+                    <strong style={{ position: "relative", top: "5px" }}>
+                      USD
+                    </strong>
+                  </p>
+                  <p style={{ position: "relative", top: "-7px" }}>
+                    United States Dollar
+                  </p>
+                </div>
+              </div>
+            )}
             {isFlagVisible && (
               <div className="currencyitem">
                 <img src={isFlagVisible} alt="" />
@@ -78,7 +136,21 @@ const Currency = () => {
                 </div>
               </div>
             )}
-            <div className="currencyitem"></div>
+            {isFlagVisible3 && (
+              <div className="currencyitem">
+                <img src={isFlagVisible3} alt="" />
+                <div className="currencyDesc">
+                  <p>
+                    <strong style={{ position: "relative", top: "5px" }}>
+                      GBP
+                    </strong>
+                  </p>
+                  <p style={{ position: "relative", top: "-7px" }}>
+                    British Pound
+                  </p>
+                </div>
+              </div>
+            )}
             <div className="currencyitem"></div>
             <div className="currencyitem"></div>
             <div className="currencyitem"></div>
